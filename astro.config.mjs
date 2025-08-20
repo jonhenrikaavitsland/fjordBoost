4bb878bbbe36725d21127c2bf375f51a0d5e1a90
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-08-21 01:56:19 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +10,6 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   site: "https://fjordboost.com"
 });
